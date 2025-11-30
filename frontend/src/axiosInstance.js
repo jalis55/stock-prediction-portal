0c4e8d5b22f6a9d1baf5a0040dd2367381364e89
jalis55 <jalismahamud2055@gmail.com>
2025-11-30 12:17:07 +0600

    axios interceptor has added

---
+++ frontend/src/axiosInstance.js
@@ -0,0 +1,51 @@
+import axios from 'axios';
+
+baseUrl = import.meta.env.VITE_BACKEND_BASE_API;
+
+const axiosInstance = axios.create({
+    baseURL: baseURL,
+    headers:{
+        'Content-Type':'application/json',
+    }
+})
+
+// Request interceptor
+axiosInstance.interceptors.request.use((config) => {
+    const access_token = localStorage.getItem('access_token');
+    if (access_token) {
+        config.headers['Authorization'] = `Bearer ${access_token}`
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+}
+)
+
+//Response interceptor
+axiosInstance.interceptors.response.use((response)=>{
+    return response
+},
+// handle failed responses
+async (error)=>{
+    const originalRequest=error.config;
+    if(error.response.status===401 && !originalRequest.retry){
+        originalRequest.retry=true;
+        const refresh_token=localStorage.getItem('refresh_token')
+        try{
+            const response=await axiosInstance.post('/token/refresh/',{refresh:refresh_token});
+            localStorage.setItem('access_token',response.data.acces);
+            originalRequest.headers['Authorization'] = `Bearer ${response.data.access}`;
+            return axiosInstance(originalRequest)
+
+        }catch(error){
+            localStorage.removeItem('access_token')
+            localStorage.removeItem('refresh_token')
+            window.location.href='/login'
+        }
+    }  
+    return Promise.reject(error);
+}
+)
+
+
+export default axiosInstance;
